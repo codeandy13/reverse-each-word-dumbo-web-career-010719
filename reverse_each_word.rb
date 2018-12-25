2d916781c43cd37acecc9331fc2de290bab327f7
Andrew Kim <akim618@gmail.com>
2018-12-25 03:14:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
 
-return string.collect{ string.split("").reverse().join("").split(" ").reverse().join(" ")}
+return string.collect string.split("").reverse().join("").split(" ").reverse().join(" ")
 
 end
