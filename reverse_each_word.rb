6e9ae477a827abb079bbee116766d6154bcf7d8a
Andrew Kim <akim618@gmail.com>
2018-12-25 02:54:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,3 @@
 def reverse_each_word(string)
-return string.split("").reverse().join("").split(" ").reverse().join(" ")
+return string.split("").reverse().join("").split(" ").reverse().join(" ").collect
 end
